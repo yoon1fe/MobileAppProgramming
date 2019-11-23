62b5000309cda802a906a63bed073d0831bdecff
yoon1fe <yoon1fe@naver.com>
2019-11-23 22:28:32 +0900

    timetable

---
+++ app/src/main/java/com/example/lectureroomReservation/Schedule_348Fragment.java
@@ -0,0 +1,32 @@
+package com.example.lectureroomReservation;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.registration.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Schedule_348Fragment extends Fragment {
+
+
+    public Schedule_348Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+
+}
